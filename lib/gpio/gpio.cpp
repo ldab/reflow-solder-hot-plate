d68593fc061c997839135af84f5f9af5d6db0ba5
ldab <l.bispo@live.com>
2023-02-27 07:03:27 +0100

    LEDs are active LOW

--- lib/gpio/gpio.cpp
+++ lib/gpio/gpio.cpp
@@ -9,33 +9,33 @@ void TaskLed(void *pvParameters)
     switch (led.colour) {
     case RED:
       digitalWrite(LED_R, !digitalRead(LED_R));
-      digitalWrite(LED_G, LOW);
-      digitalWrite(LED_B, LOW);
+      digitalWrite(LED_G, HIGH);
+      digitalWrite(LED_B, HIGH);
       break;
     case GREEN:
-      digitalWrite(LED_R, LOW);
+      digitalWrite(LED_R, HIGH);
       digitalWrite(LED_G, !digitalRead(LED_G));
-      digitalWrite(LED_B, LOW);
+      digitalWrite(LED_B, HIGH);
       break;
     case BLUE:
-      digitalWrite(LED_R, LOW);
-      digitalWrite(LED_G, LOW);
+      digitalWrite(LED_R, HIGH);
+      digitalWrite(LED_G, HIGH);
       digitalWrite(LED_B, !digitalRead(LED_B));
       break;
     case CYAN:
-      digitalWrite(LED_R, LOW);
+      digitalWrite(LED_R, HIGH);
       digitalWrite(LED_G, !digitalRead(LED_G));
       digitalWrite(LED_B, !digitalRead(LED_B));
       break;
     case PURPLE:
       digitalWrite(LED_R, !digitalRead(LED_R));
-      digitalWrite(LED_G, LOW);
+      digitalWrite(LED_G, HIGH);
       digitalWrite(LED_B, !digitalRead(LED_B));
       break;
     case YELLOW:
       digitalWrite(LED_R, !digitalRead(LED_R));
       digitalWrite(LED_G, !digitalRead(LED_G));
-      digitalWrite(LED_B, LOW);
+      digitalWrite(LED_B, HIGH);
       break;
     case WHITE:
       digitalWrite(LED_R, !digitalRead(LED_R));
@@ -43,9 +43,9 @@ void TaskLed(void *pvParameters)
       digitalWrite(LED_B, !digitalRead(LED_B));
       break;
     default:
-      digitalWrite(LED_R, LOW);
-      digitalWrite(LED_G, LOW);
-      digitalWrite(LED_B, LOW);
+      digitalWrite(LED_R, HIGH);
+      digitalWrite(LED_G, HIGH);
+      digitalWrite(LED_B, HIGH);
       break;
     }
     vTaskDelay(pdMS_TO_TICKS(1000 / led.rate));
@@ -65,7 +65,7 @@ void gpios_init(void)
   pinMode(LED_G, OUTPUT);
   pinMode(LED_B, OUTPUT);
 
-  set_led(CYAN, SLOW);
+  set_led(WHITE, SLOW);
 
   xTaskCreate(TaskLed, "TaskLed", 1024, NULL, 0, NULL);
 }
