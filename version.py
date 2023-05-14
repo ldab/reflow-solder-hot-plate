a6da7e04b1874343090422d9d7e96bd5cc32323c
ldab <l.bispo@live.com>
2023-05-14 15:09:05 +0200

    git tag

--- version.py
+++ version.py
@@ -3,10 +3,10 @@ import subprocess
 Import("env")
 
 def get_firmware_specifier_build_flag():
-    ret = subprocess.run(["git", "describe", "--always", "--dirty"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
+    ret = subprocess.run(["git", "describe", "--tags", "--always", "--dirty"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
     build_version = ret.stdout.strip()
     build_flag = "-D VERSION=\\\"" + build_version + "\\\""
-    print ("Firmware Revision: " + build_version)
+    print ("Firmware Version: " + build_version)
     return (build_flag)
 
 env.Append(
